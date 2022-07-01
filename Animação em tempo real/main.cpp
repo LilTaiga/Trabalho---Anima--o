401dec863dcd7b0808464bb257e48450a8968791
LilTaiga <liltaigaishappy@gmail.com>
2022-07-01 01:46:53 -0300

    Adicionado comentários

---
+++ Animação em tempo real/main.cpp
@@ -0,0 +1,108 @@
+// Aqui é onde exibimos o contexto para visualização.
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <iostream>
+#include <stdio.h>
+#include "game.h"
+
+using namespace std;
+
+#define COLUMNS 40
+#define ROWS 40
+#define FPS 10
+#define WIDTH 750
+#define HEIGHT 750
+
+void init();
+void displayCallback();
+void reshapeCallback(int, int);
+void timerCallback(int);
+void keybordCallback(unsigned char, int, int);
+
+// Inicialização do openGL
+// Criação da janela
+// Definição das funções de callback
+int main(int argc, char** argv)
+{
+	// Inicialização do glut
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - WIDTH) / 2,
+		(glutGet(GLUT_SCREEN_HEIGHT) - HEIGHT) / 2);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("ANIMATION");
+	init();
+
+	// Aqui uma função de timer é declarada.
+	// Como aqui é uma animação em em tempo real,
+	// precisamos desenhar na tela a imagem renderizada a cada 1 / FPS segundos.
+	glutTimerFunc(0, timerCallback, 0);
+	glutReshapeFunc(reshapeCallback);
+
+	// A função de renderização e desenho do render na tela.
+	glutDisplayFunc(displayCallback);
+	glutKeyboardFunc(keybordCallback);
+	glutMainLoop();
+	return 0;
+}
+
+// Aqui é realizado os preparativos para renderizar o mundo.
+void init()
+{
+	glClearColor(0.792, 0.792, 0.792, 1.0);
+	initGrid(COLUMNS, ROWS);
+}
+
+// Aqui é onde a camera faz o render do mundo.
+// O render também é exibido na tela aqui ???????
+void displayCallback()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	// Aqui a camera faz o render do mundo.
+	drawGrid();
+	drawBotamon();
+
+	// Aqui o render é exibido na tela ????????
+	glutSwapBuffers();
+}
+
+void reshapeCallback(int w, int h)
+{
+	if (w >= h)
+	{
+		glViewport(((GLsizei)w - (GLsizei)h) / 2, 0, (GLsizei)h, (GLsizei)h);
+	}
+	else if (w < h)
+	{
+		glViewport(0, ((GLsizei)h - (GLsizei)w) / 2, (GLsizei)w, (GLsizei)w);
+	}
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, COLUMNS, 0.0, ROWS, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+// Aqui é um timer para re-renderizar o mundo a cada 1 / FPS segundos.
+// A re-renderização também é exibida na tela pela função de display.
+void timerCallback(int)
+{
+	glutPostRedisplay();
+	glutTimerFunc(1000 / FPS, timerCallback, 0);
+}
+
+// Intercepta comandos do teclado.
+// Não faz nada além de fechar a janela caso ESC tenha sido pressionado.
+void keybordCallback(unsigned char key, int, int)
+{
+	switch (key)
+	{
+	case 27:
+		exit(0);
+		break;
+
+	default:
+		break;
+	}
+}
